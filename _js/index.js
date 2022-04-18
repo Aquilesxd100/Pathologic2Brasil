c993eb8379c61314ccc4e6e89611b3c3d7896e0a
Aquilesxd <fernandoalanfillmann@gmail.com>
2022-04-18 16:31:54 -0300

    Transicao BASICA da Barra de Pesquisa

--- _js/index.js
+++ _js/index.js
@@ -12,6 +12,20 @@ function diversosLink () {
 function maisTutoriais_GuiasLink () {
     window.location.href="https://www.google.com/";
 }
+/* Barra de Pesquisa */
+let barraPesquisaAbertura = 0;
+    function barraPesquisa() {
+        if (barraPesquisaAbertura === 0) {
+            document.getElementById("barrapesquisa").src="_imagens/barra_pesquisa.png";
+            document.getElementById("barrapesquisa").style.left="77.2vw";
+            barraPesquisaAbertura = 1;
+        }
+        else {
+            document.getElementById("barrapesquisa").src="_imagens/icone_pesquisa.png";
+            document.getElementById("barrapesquisa").style.left="93vw";
+            barraPesquisaAbertura = 0;  
+        }
+    }
 /* Hover */
     function menuHoverIn(barra) {  
         document.getElementById(barra).style.color = "#FFFFFF";
