2ba8ae66d8c89319d366e811b928a1b32e2aca90
Aquilesxd <fernandoalanfillmann@gmail.com>
2022-07-23 20:07:12 -0300

    Finalização Pagína Mapa Útil

--- _js/mapa.js
+++ _js/mapa.js
@@ -19,3 +19,32 @@ function mapaZoom(tipo) {
         document.getElementById("mapa-botao-fechar2").style.right="-24vh";
     }
 }
+var ultimaSecaoInfo = "";
+function secaoInfos(secao, titulo) {
+    telaLargura = window.innerWidth;
+    function opacidadeConteudo() {
+        document.getElementById(secao).style.opacity="1";
+    }
+    if (ultimaSecaoInfo !== "" && ultimaSecaoInfo !== secao) {
+        document.getElementById(ultimaSecaoInfo).style.opacity="0";
+        document.getElementById(ultimaSecaoInfo).style.display="none";
+    }
+    document.getElementById(secao).style.display="flex";
+    if (secao === "depositos-secretos" && telaLargura <= 800) {
+        document.getElementById("info-marcador").style.maxHeight="7500px";
+    }
+    else {
+        document.getElementById("info-marcador").style.maxHeight="4000px";
+    }
+    document.getElementById("marcador-titulo").innerHTML=titulo;
+    document.getElementById("marcador-titulo-div").style.padding="20px 0 24px 0";
+    document.getElementById("marcador-titulo").style.letterSpacing="0.05vw";
+    if (telaLargura <= 800) {
+        document.getElementById("marcador-titulo").style.fontSize="34px";
+    }
+    else {
+        document.getElementById("marcador-titulo").style.fontSize="40px";
+    }
+    setTimeout(opacidadeConteudo, 300);
+    ultimaSecaoInfo = secao;
+}
