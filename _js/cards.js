36a42fc3e76a1f9aa15728d089277dc6cc5be09f
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-02-28 16:10:33 -0300

    Fix bug pesquisa imprevisivel

--- _js/cards.js
+++ _js/cards.js
@@ -48,7 +48,7 @@ if (articlePesquisa) {
     ];
     // -------------------------------------------------------------------- //
     const resulParams = new URLSearchParams(window.location.search);
-    const resultadoPesquisa = resulParams.get(`resultados`);
+    const resultadoPesquisa = (resulParams.get(`resultados`)).toLowerCase();
     const inputPesquisa = document.querySelector(`#input`);
     const inputPesquisaMobile = document.querySelector(`#input-mobile`);
     inputPesquisa.value = resultadoPesquisa;
