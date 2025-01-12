7c58527d6ed8e9e2e3e16a526c847d224f760850
fernando <fernandoalanfillmann@gmail.com>
2025-01-12 16:52:26 -0300

    FEAT: Versao EN dos textos do guia de receitas

--- _js/receitas.js
+++ _js/receitas.js
@@ -8,6 +8,9 @@ const orgaosSangueClasse2 = document.getElementsByClassName("orgao-sangue-combin
 const analgesicosCoresInfusoes = document.getElementsByClassName("analgesicos-infusao-cor");
 const analgesicosCoresAnalgesicos = document.getElementsByClassName("analgesicos-analgesico-cor");
 const analgesicosEfeitos = document.getElementsByClassName("analgesicos-efeito");
+
+const caminhoBaseIcone = (!paginaEstaEmPortugues ? "../" : "") + "_imagens/_icones/";
+
 function rotacaoItens() {
     let ervasComuns = ["blacktwyre", "bloodtwyre", "twyre"];
     let ervasRaras = ["ashenswish", "whitewhip", "swevery"];
@@ -29,49 +32,49 @@ function rotacaoItens() {
             }
     }
     if (document.getElementById("ervas-comuns").getBoundingClientRect().top < telaAltura && document.getElementById("ervas-comuns").getBoundingClientRect().bottom > 0) {
-        document.getElementById("ervas-comuns").innerHTML="<img src='_imagens/_icones/" + ervasComuns[contador3[0]] + ".jpg'>";
+        document.getElementById("ervas-comuns").innerHTML="<img src='" + caminhoBaseIcone + ervasComuns[contador3[0]] + ".jpg'>";
         contador3[0] = contador3[0] + 1;
     }
     if (document.getElementById("ervas-raras").getBoundingClientRect().top < telaAltura && document.getElementById("ervas-raras").getBoundingClientRect().bottom > 0) {
-        document.getElementById("ervas-raras").innerHTML="<img src='_imagens/_icones/" + ervasRaras[contador3[1]] + ".jpg'>";
+        document.getElementById("ervas-raras").innerHTML="<img src='" + caminhoBaseIcone + ervasRaras[contador3[1]] + ".jpg'>";
         contador3[1] = contador3[1] + 1;
     }
     if (document.getElementById("ervas-todas").getBoundingClientRect().top < telaAltura && document.getElementById("ervas-todas").getBoundingClientRect().bottom > 0) {
-        document.getElementById("ervas-todas").innerHTML="<img src='_imagens/_icones/" + ervasComuns[contador3[2]] + ".jpg'>";
+        document.getElementById("ervas-todas").innerHTML="<img src='" + caminhoBaseIcone + ervasComuns[contador3[2]] + ".jpg'>";
         contador3[2] = contador3[2] + 1;
     }  
     for (let c = 0; c < 3; c++) {
         if (ervasComunsClasse[c].getBoundingClientRect().top < telaAltura && ervasComunsClasse[c].getBoundingClientRect().bottom > 0) {
             for (let c2 = 0; c2 < 3; c2++) {
-                ervasComunsClasse[c2].innerHTML="<img src='_imagens/_icones/" + ervasComuns[contador3[3]] + ".jpg'>";
+                ervasComunsClasse[c2].innerHTML="<img src='" + caminhoBaseIcone + ervasComuns[contador3[3]] + ".jpg'>";
             }
             contador3[3] = contador3[3] + 1;
             c = 3;
         } 
     }
     if (document.getElementById("infusoes-normais").getBoundingClientRect().top < telaAltura && document.getElementById("infusoes-normais").getBoundingClientRect().bottom > 0) {
-        document.getElementById("infusoes-normais").innerHTML="<img src='_imagens/_icones/" + infusoesNormais[contador3[4]] + ".jpg'>";
+        document.getElementById("infusoes-normais").innerHTML="<img src='" + caminhoBaseIcone + infusoesNormais[contador3[4]] + ".jpg'>";
         contador3[4] = contador3[4] + 1;
     }  
     if (document.getElementById("orgao-sangue-infectado").getBoundingClientRect().top < telaAltura && document.getElementById("orgao-sangue-infectado").getBoundingClientRect().bottom > 0) {
-        document.getElementById("orgao-sangue-infectado").innerHTML="<img src='_imagens/_icones/" + orgaosSangueInfectado[contador5[0]] + ".jpg'>";
+        document.getElementById("orgao-sangue-infectado").innerHTML="<img src='" + caminhoBaseIcone + orgaosSangueInfectado[contador5[0]] + ".jpg'>";
         contador5[0] = contador5[0] + 1;
     }  
     for (let c = 0; c < 3; c++) {
         if (orgaosSangueClasse[c].getBoundingClientRect().top < telaAltura && orgaosSangueClasse[c].getBoundingClientRect().bottom > 0) {
             for (let c2 = 0; c2 < 3; c2++) {
-                orgaosSangueClasse[c2].innerHTML="<img src='_imagens/_icones/" + orgaosSangueInfectado[contador5[1]] + ".jpg'>";
+                orgaosSangueClasse[c2].innerHTML="<img src='" + caminhoBaseIcone + orgaosSangueInfectado[contador5[1]] + ".jpg'>";
             }
             contador5[1] = contador5[1] + 1;
             c = 5;
         } 
     }
     if (document.getElementById("infusoes-normais2").getBoundingClientRect().top < telaAltura && document.getElementById("infusoes-normais2").getBoundingClientRect().bottom > 0) {
-        document.getElementById("infusoes-normais2").innerHTML="<img src='_imagens/_icones/" + infusoesNormais[contador3[5]] + ".jpg'>";
+        document.getElementById("infusoes-normais2").innerHTML="<img src='" + caminhoBaseIcone + infusoesNormais[contador3[5]] + ".jpg'>";
         contador3[5] = contador3[5] + 1;
     }  
     if (document.getElementById("orgao-sangue-saudavel").getBoundingClientRect().top < telaAltura && document.getElementById("orgao-sangue-saudavel").getBoundingClientRect().bottom > 0) {
-        document.getElementById("orgao-sangue-saudavel").innerHTML="<img src='_imagens/_icones/" + orgaosSangueSaudavel[contador5[2]] + ".jpg'>";
+        document.getElementById("orgao-sangue-saudavel").innerHTML="<img src='" + caminhoBaseIcone + orgaosSangueSaudavel[contador5[2]] + ".jpg'>";
         contador5[2] = contador5[2] + 1;
     }  
     setTimeout(rotacaoItens, 1100);
@@ -80,9 +83,22 @@ function rotacaoItens2() {
     let corAnalgesicoCombinacaoMelhor = ["yas", "medrel", "yas", "medrel", "zurkh"];
     let corAnalgesicoCombinacaoMedio = ["medrel", "zurkh", "zurkh", "zurkh", "yas"];
     let corAnalgesicoCombinacaoPior = ["zurkh", "yas", "medrel", "yas", "medrel"];
-    let efeitoAnalgesicoCombinacaoMelhor = ["+10% Imunidade", "<span>+45% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-15% Fome", "-15% Exaustão", "-20% de Sede"];
-    let efeitoAnalgesicoCombinacaoMedio = ["+7.5% Imunidade", "<span>+30% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-10% Fome", "-10% Exaustão", "-15% de Sede"];
-    let efeitoAnalgesicoCombinacaoPior = ["+5% Imunidade", "<span>+15% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-5% Fome", "-5% Exaustão", "-10% de Sede"];
+
+    let efeitoAnalgesicoCombinacaoMelhor = 
+        paginaEstaEmPortugues
+            ? ["+10% Imunidade", "<span>+45% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-15% Fome", "-15% Exaustão", "-20% de Sede"]
+            : ["+10% Immunity", "<span>+45% Exhaustion regeneration <span class='sem-quebra'>(while sleeping)</span></span>", "-15% Hunger", "-15% Exhaustion", "-20% Thirst"];
+
+    let efeitoAnalgesicoCombinacaoMedio = 
+        paginaEstaEmPortugues
+            ? ["+7.5% Imunidade", "<span>+30% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-10% Fome", "-10% Exaustão", "-15% de Sede"]
+            : ["+7.5% Immunity", "<span>+30% Exhaustion regeneration <span class='sem-quebra'>(while sleeping)</span></span>", "-10% Hunger", "-10% Exhaustion", "-15% Thirst"];
+
+    let efeitoAnalgesicoCombinacaoPior = 
+        paginaEstaEmPortugues
+            ? ["+5% Imunidade", "<span>+15% Regeneração de Exaustão <span class='sem-quebra'>(ao dormir)</span></span>", "-5% Fome", "-5% Exaustão", "-10% de Sede"]
+            :  ["+5% Immunity", "<span>+15% Exhaustion regeneration <span class='sem-quebra'>(while sleeping)</span></span>", "-5% Hunger", "-5% Exhaustion", "-10% Thirst"];
+
     let orgaosSangueSaudavel = ["rim", "figado", "coracao", "cerebro", "sangue"];
     let telaAltura = window.innerHeight;
     if (contador5[3] === 5) {
@@ -91,16 +107,16 @@ function rotacaoItens2() {
     for (let c = 0; c < 3; c++) {
         if (orgaosSangueClasse2[c].getBoundingClientRect().top < telaAltura && orgaosSangueClasse2[c].getBoundingClientRect().bottom > 0) {
             for (let c2 = 0; c2 < 3; c2++) {
-                orgaosSangueClasse2[c2].innerHTML="<img src='_imagens/_icones/" + orgaosSangueSaudavel[contador5[3]] + ".jpg'>";
+                orgaosSangueClasse2[c2].innerHTML="<img src='" + caminhoBaseIcone + orgaosSangueSaudavel[contador5[3]] + ".jpg'>";
             }
-            analgesicosCoresInfusoes[0].innerHTML="<img src='_imagens/_icones/" + corAnalgesicoCombinacaoMelhor[contador5[3]] + ".jpg'>";
-            analgesicosCoresAnalgesicos[0].innerHTML="<img src='_imagens/_icones/analgesico_" + corAnalgesicoCombinacaoMelhor[contador5[3]] + ".jpg'>";
+            analgesicosCoresInfusoes[0].innerHTML="<img src='" + caminhoBaseIcone + corAnalgesicoCombinacaoMelhor[contador5[3]] + ".jpg'>";
+            analgesicosCoresAnalgesicos[0].innerHTML="<img src='" + caminhoBaseIcone + "analgesico_" + corAnalgesicoCombinacaoMelhor[contador5[3]] + ".jpg'>";
             analgesicosEfeitos[0].innerHTML="<h2>" + efeitoAnalgesicoCombinacaoMelhor[contador5[3]] + "</h2>";   
-            analgesicosCoresInfusoes[1].innerHTML="<img src='_imagens/_icones/" + corAnalgesicoCombinacaoMedio[contador5[3]] + ".jpg'>";
-            analgesicosCoresAnalgesicos[1].innerHTML="<img src='_imagens/_icones/analgesico_" + corAnalgesicoCombinacaoMedio[contador5[3]] + ".jpg'>";
+            analgesicosCoresInfusoes[1].innerHTML="<img src='" + caminhoBaseIcone + corAnalgesicoCombinacaoMedio[contador5[3]] + ".jpg'>";
+            analgesicosCoresAnalgesicos[1].innerHTML="<img src='" + caminhoBaseIcone + "analgesico_" + corAnalgesicoCombinacaoMedio[contador5[3]] + ".jpg'>";
             analgesicosEfeitos[1].innerHTML="<h2>" + efeitoAnalgesicoCombinacaoMedio[contador5[3]] + "</h2>";
-            analgesicosCoresInfusoes[2].innerHTML="<img src='_imagens/_icones/" + corAnalgesicoCombinacaoPior[contador5[3]] + ".jpg'>";
-            analgesicosCoresAnalgesicos[2].innerHTML="<img src='_imagens/_icones/analgesico_" + corAnalgesicoCombinacaoPior[contador5[3]] + ".jpg'>";
+            analgesicosCoresInfusoes[2].innerHTML="<img src='" + caminhoBaseIcone + corAnalgesicoCombinacaoPior[contador5[3]] + ".jpg'>";
+            analgesicosCoresAnalgesicos[2].innerHTML="<img src='" + caminhoBaseIcone + "analgesico_" + corAnalgesicoCombinacaoPior[contador5[3]] + ".jpg'>";
             analgesicosEfeitos[2].innerHTML="<h2>" + efeitoAnalgesicoCombinacaoPior[contador5[3]] + "</h2>";
             contador5[3] = contador5[3] + 1;
             c = 5;
